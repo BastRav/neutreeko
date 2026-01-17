4940fbdc31049069c65c5cfa478012947eede6f8
Bastien Ravot <bastien.ravot@sfr.fr>
2026-01-17 16:43:22 +0100

    WIP

--- src/ai/ann/train.rs
+++ src/ai/ann/train.rs
@@ -1,9 +1,18 @@
-use super::{ANN, ANNConfig, PolicyValueOutput};
+use super::{
+    ANN, PolicyValueOutput,
+    inputouput::{board_to_input, output_to_moves, illegal_mask},
+};
 use burn::{
     nn::loss::{MseLoss, Reduction},
     optim::{Adam, AdamConfig, GradientsParams, Optimizer, adaptor::OptimizerAdaptor},
     tensor::{Device, Tensor, activation::log_softmax, backend::AutodiffBackend},
 };
+use crate::{
+    ai::{AI, alphazeutreeko::AlphaZeutreeko,},
+    logic::{Board, Color},
+};
+
+use log::info;
 
 pub struct PolicyValueTarget<B: AutodiffBackend> {
     pub value: Tensor<B, 1>,
@@ -11,7 +20,7 @@ pub struct PolicyValueTarget<B: AutodiffBackend> {
 }
 
 pub struct ANNTrainer<B: AutodiffBackend> {
-    pub model: ANN<B>,
+    pub alphazeutreeko: AlphaZeutreeko<B>,
     pub optimizer: OptimizerAdaptor<Adam, ANN<B>, B>,
     pub device: Device<B>,
 }
@@ -19,11 +28,11 @@ pub struct ANNTrainer<B: AutodiffBackend> {
 impl<B: AutodiffBackend<FloatElem = f32>> ANNTrainer<B> {
     pub fn new() -> Self {
         let device = B::Device::default();
-        let model = ANNConfig::init(32, &device);
         let optimizer = AdamConfig::new().init();
+        let alphazeutreeko = AlphaZeutreeko::new(Color::Green, 1);
 
         Self {
-            model,
+            alphazeutreeko,
             optimizer,
             device,
         }
@@ -39,22 +48,28 @@ impl<B: AutodiffBackend<FloatElem = f32>> ANNTrainer<B> {
 
     pub fn train_step(&mut self, input:Tensor<B, 3>, target: PolicyValueTarget<B>, illegal_mask: Tensor<B, 2>) -> Tensor<B, 1> {
         // Forward pass
-        let output = self.model.forward(input);
+        let output = self.alphazeutreeko.mcts.policy.ann.forward(input);
 
         let loss = self.loss(output, target, illegal_mask);
         let grads = loss.backward();
-        let grads = GradientsParams::from_grads(grads, &self.model);
+        let grads = GradientsParams::from_grads(grads, &self.alphazeutreeko.mcts.policy.ann);
 
-        // Update model parameters
-        self.model = self.optimizer.step(0.001, self.model.clone(), grads);
+        // Update self.alphazeutreeko.policy.ann parameters
+        self.alphazeutreeko.mcts.policy.ann = self.optimizer.step(0.001, self.alphazeutreeko.mcts.policy.ann.clone(), grads);
         loss
     }
 
-    pub fn save(&self, _path: &str) {
-        // Implement model saving
-    }
+    pub fn training_loop(&mut self, max_epoch: i32) {
+        for epoch in 1..=max_epoch {
+            info!("Starting iteration {}", epoch);
+            let mut board = Board::default_new();
+            while board.winner().is_none(){
+                
 
-    pub fn load(&mut self, _path: &str) {
-        // Implement model loading
+                let input = board_to_input(&board, &self.device);
+                let illegal_mask = illegal_mask(&board, &self.device);
+                self.train_step(input, target, illegal_mask);
+            }
+        }
     }
 }
--- src/train.rs
+++ src/train.rs
@@ -1,32 +1,8 @@
-use neutreeko::{
-    ai::ann::{
-        inputouput::{board_to_input, illegal_mask},
-        train::{ANNTrainer, PolicyValueTarget},
-    },
-    logic::Board,
-};
-use burn::{
-    backend::{Autodiff, NdArray},
-    tensor::Tensor,
-};
+use neutreeko::ai::ann::train::ANNTrainer;
+use burn::backend::{Autodiff, NdArray};
 
 #[cfg(feature = "train")]
 fn main() {
     let mut trainer: ANNTrainer<Autodiff<NdArray<f32>>> = ANNTrainer::new();
-
-    // Example training loop
-    let board = Board::default_new();
-    let value_target = 0.5; // Example target value
-    let policy_target = [0.1, 0.2, 0.7, 0.0]; // Example move probabilities
-
-    let value_target = Tensor::from_floats([value_target], &trainer.device);
-    let policy_target = Tensor::from_floats(policy_target, &trainer.device);
-    let target = PolicyValueTarget {
-        value: value_target,
-        policy: policy_target,
-    };
-
-    let input = board_to_input(&board, &trainer.device);
-    let illegal_mask = illegal_mask(&board, &trainer.device);
-    trainer.train_step(input, target, illegal_mask);
+    trainer.training_loop();
 }
