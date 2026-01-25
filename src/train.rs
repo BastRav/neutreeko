1f7d1495ac92b8556bbd9c7df60fb6d42b7fe0d5
Bastien Ravot <bastien.ravot@sfr.fr>
2026-01-25 17:08:54 +0100

    New model, finally 1 victory against MinMax4

--- src/train.rs
+++ src/train.rs
@@ -11,49 +11,50 @@ use burn::backend::{Autodiff, NdArray};
 
 #[cfg(feature = "train")]
 fn main() {
-    // train();
+    train();
     evaluate();
 }
 
 fn train() {
     let mut trainer: ANNTrainer<Autodiff<NdArray<f32>>, MinMax<NativePlatform>> = ANNTrainer::new();
-    // let _ = trainer.load("assets/models/3_100_MinMax6");
+    let result = trainer.load("assets/models/12_3_opening");
+    if result.is_err() {println!("Could not load model");}
+
+    // trainer.train_opening(3);
+    // let _ = trainer.save("assets/models/7_3_opening");
+
+    // trainer.opponent = Some(MinMax::new(Color::Yellow, 4));
+    // trainer.training_loop(10);
+    // let _ = trainer.save("assets/models/8_10_MinMax4");
+
+    // trainer.training_loop(10);
+    // let _ = trainer.save("assets/models/9_10_MinMax4");
+
+    // trainer.training_loop(10);
+    // let _ = trainer.save("assets/models/10_10_MinMax4");
+
+    // trainer.opponent = None;
+    trainer.training_loop(200);
+    let _ = trainer.save("assets/models/13_200_itself");
 
     trainer.train_opening(3);
-    let _ = trainer.save("assets/models/1_3_opening");
-
-    trainer.opponent = Some(MinMax::new(Color::Yellow, 4));
-    trainer.training_loop(10);
-    let _ = trainer.save("assets/models/2_10_MinMax4");
-
-    trainer.training_loop(10);
-    let _ = trainer.save("assets/models/3_10_MinMax4");
-
-    trainer.training_loop(10);
-    let _ = trainer.save("assets/models/4_10_MinMax4");
-
-    trainer.opponent = None;
-    trainer.training_loop(10);
-    let _ = trainer.save("assets/models/5_10_itself");
-
-    trainer.train_opening(1);
-    let _ = trainer.save("assets/models/6_1_opening");
+    let _ = trainer.save("assets/models/14_3_opening");
     trainer.save_for_web();
 }
 
 fn evaluate(){
     let mut trainer: ANNTrainer<Autodiff<NdArray<f32>>, MinMax<NativePlatform>> = ANNTrainer::new();
     trainer.opponent = Some(MinMax::new(Color::Yellow, 4));
-    let _ = trainer.load("assets/models/1_3_opening");
+    // let _ = trainer.load("assets/models/7_3_opening");
+    // trainer.evaluate(2);
+    // let _ = trainer.load("assets/models/8_10_MinMax4");
+    // trainer.evaluate(2);
+    // let _ = trainer.load("assets/models/9_10_MinMax4");
+    // trainer.evaluate(2);
+    // let _ = trainer.load("assets/models/10_10_MinMax4");
+    // trainer.evaluate(2);
+    let _ = trainer.load("assets/models/13_200_itself");
     trainer.evaluate(2);
-    let _ = trainer.load("assets/models/2_10_MinMax4");
-    trainer.evaluate(2);
-    let _ = trainer.load("assets/models/3_10_MinMax4");
-    trainer.evaluate(2);
-    let _ = trainer.load("assets/models/4_10_MinMax4");
-    trainer.evaluate(2);
-    let _ = trainer.load("assets/models/5_10_itself");
-    trainer.evaluate(2);
-    let _ = trainer.load("assets/models/6_1_opening");
+    let _ = trainer.load("assets/models/14_3_opening");
     trainer.evaluate(2);
 }
